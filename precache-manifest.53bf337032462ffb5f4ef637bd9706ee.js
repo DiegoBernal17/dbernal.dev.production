308fb20d1b3874f86ff5cd41746bbcbf713d7613
Diego P. Bernal <diego.bernal17@gmail.com>
2019-07-01 14:21:07 -0500

    Producction

---
+++ precache-manifest.53bf337032462ffb5f4ef637bd9706ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6392f56a9023c9e00f2",
+    "url": "/static/css/main.b8d66a90.chunk.css"
+  },
+  {
+    "revision": "e6392f56a9023c9e00f2",
+    "url": "/static/js/main.67abe355.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68cfcf619ea91c0b7487",
+    "url": "/static/css/2.ed1c871f.chunk.css"
+  },
+  {
+    "revision": "68cfcf619ea91c0b7487",
+    "url": "/static/js/2.f35138f1.chunk.js"
+  },
+  {
+    "revision": "a8c9302c63e20a6a712c498acd4ae1a3",
+    "url": "/index.html"
+  }
+];
